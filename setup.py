2023080abaa10a683dd807cd79d828c8698bbf9a
DmitryMurashov <dmtr.murashov@gmail.com>
2023-06-17 15:51:54 +0300

    build

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup
 
+def get_long_description():
+    with open("README.md", 'r', encoding='utf-8') as f:
+        return f.read()
+
 setup(
     name="tired",
     packages=[
@@ -8,6 +12,7 @@ setup(
     include_package_data=True,
     license="MIT",
     description="Boilerplate I'm tired of writing over and over",
+    long_description=get_long_description(),
     author="Dmitry Murashov",
     setup_requires=["wheel"],
     install_requires=[
@@ -16,7 +21,7 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Inependent",
+        "Operating System :: OS Independent",
     ],
     python_requires=">=3.7",
     version="0.0.1",
