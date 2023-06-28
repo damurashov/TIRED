ad8137ffa8861d0eb1b14a1f3acbc36b85d256b6
DmitryMurashov <dmtr.murashov@gmail.com>
2023-06-28 12:17:48 +0300

    [tired:fs] Impl |

---
+++ tired/fs.py
@@ -0,0 +1,16 @@
+def iterate_directory_content(directory: str):
+    """
+    Iterate only through directory content
+    """
+    import os
+
+    content = os.listdir(directory)
+
+    return content
+
+
+def iterate_directory_content_directories(directory: str):
+    import os
+
+    return filter(
+        lambda item: os.path.isdir(item), iterate_directory_content(directory))
