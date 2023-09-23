61efe2458ed61b8220f380eb417dee53943ce4c3
DmitryMurashov <dmtr.murashov@gmail.com>
2023-09-23 16:19:09 +0300

    [tired:datetime] Impl | Current time

--- tired/datetime.py
+++ tired/datetime.py
@@ -8,3 +8,9 @@ def get_today_string_date():
     current_date = datetime.datetime.strftime(datetime.datetime.now(), DATE_FORMAT)
 
     return current_date
+
+
+def get_today_string_time():
+    current_date = datetime.datetime.strftime(datetime.datetime.now(), TIME_FORMAT)
+
+    return current_date
