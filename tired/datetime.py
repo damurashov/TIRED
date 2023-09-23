0dfe3af01a0c88191fba3c646c0ece251e67f76c
DmitryMurashov <dmtr.murashov@gmail.com>
2023-09-23 22:24:05 +0300

    [tired:datetime] Bug | Format

--- tired/datetime.py
+++ tired/datetime.py
@@ -3,7 +3,7 @@ import datetime
 DATE_FORMAT = "%Y-%m-%d"
 TIME_FORMAT = "%Y-%m-%d %H:%M"
 TIME_FORMAT_SECONDS = "%Y-%m-%d %H:%M:%S"
-TIME_FORMAT_MILLISECONDS = "%Y-%m-%d %H:%M:%S:%sss"
+TIME_FORMAT_MILLISECONDS = "%Y-%m-%d %H:%M:%S:%f"
 
 
 def get_today_date_string():
