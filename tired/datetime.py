859408c03632737a333b32f32ee64d87a6f07c6e
DmitryMurashov <dmtr.murashov@gmail.com>
2023-09-23 22:22:27 +0300

    [tired:datetime] Impl | Milliseconds

--- tired/datetime.py
+++ tired/datetime.py
@@ -3,6 +3,7 @@ import datetime
 DATE_FORMAT = "%Y-%m-%d"
 TIME_FORMAT = "%Y-%m-%d %H:%M"
 TIME_FORMAT_SECONDS = "%Y-%m-%d %H:%M:%S"
+TIME_FORMAT_MILLISECONDS = "%Y-%m-%d %H:%M:%S:%sss"
 
 
 def get_today_date_string():
@@ -19,5 +20,11 @@ def get_today_time_string():
 
 def get_today_time_seconds_string():
     current_date = datetime.datetime.strftime(datetime.datetime.now(), TIME_FORMAT_SECONDS)
+
+    return current_date
+
+
+def get_today_time_milliseconds_string():
+    current_date = datetime.datetime.strftime(datetime.datetime.now(), TIME_FORMAT_MILLISECONDS)
 
     return current_date
