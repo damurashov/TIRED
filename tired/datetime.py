785d25d9a1eaf1cbcb194092f1192ed0e6da43f7
DmitryMurashov <dmtr.murashov@gmail.com>
2023-09-23 16:21:45 +0300

    [tired:datetime] Impl | Exact time

--- tired/datetime.py
+++ tired/datetime.py
@@ -2,15 +2,22 @@ import datetime
 
 DATE_FORMAT = "%Y-%m-%d"
 TIME_FORMAT = "%Y-%m-%d %H:%M"
+TIME_FORMAT_SECONDS = "%Y-%m-%d %H:%M"
 
 
-def get_today_string_date():
+def get_today_date_string():
     current_date = datetime.datetime.strftime(datetime.datetime.now(), DATE_FORMAT)
 
     return current_date
 
 
-def get_today_string_time():
+def get_today_time_string():
+    current_date = datetime.datetime.strftime(datetime.datetime.now(), TIME_FORMAT)
+
+    return current_date
+
+
+def get_today_time_seconds_string():
     current_date = datetime.datetime.strftime(datetime.datetime.now(), TIME_FORMAT)
 
     return current_date
