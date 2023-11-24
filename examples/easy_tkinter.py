c95de856284ff347393770d861be41305ca006cf
DmitryMurashov <dmtr.murashov@gmail.com>
2023-11-24 13:01:44 +0300

    [examples:easy_tkinter] Impl | Extended the example with the use of tabs

--- examples/easy_tkinter.py
+++ examples/easy_tkinter.py
@@ -1,15 +1,21 @@
 import tired.tk
 import tkinter
+from tkinter import ttk
 
 def main():
 	window = tkinter.Tk()
-	frame = tired.tk.Frame(window)
+	tabs = ttk.Notebook(window)
+	frame = tired.tk.Frame(tabs)
 	frame.add_checkbox("Have some rest", True)
 	frame.add_file_selection("Select some file")
 	frame.add_spinbox("Select value", 0, 100, 20)
 	frame.add_button("Apply")
+	simple_frame = tired.tk.Frame(tabs)
+	simple_frame.add_button("Just run")
+	tabs.add(frame, text="Complicated frame")
+	tabs.add(simple_frame, text="Simple frame")
 	window.title("So TIRED...")
-	frame.pack(expand=True, fill='x')
+	tabs.pack(expand=True, fill='both')
 	window.mainloop()
 
 
