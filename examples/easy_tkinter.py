f6d727dde37a0c58e2211a63e43594565c6d3be7
DmitryMurashov <dmtr.murashov@gmail.com>
2023-11-24 12:21:14 +0300

    [examples:easy_tkinter] Impl | Spinbox example

--- examples/easy_tkinter.py
+++ examples/easy_tkinter.py
@@ -6,6 +6,7 @@ def main():
 	frame = tired.tk.Frame(window)
 	frame.add_checkbox("Have some rest", True)
 	frame.add_file_selection("Select some file")
+	frame.add_spinbox("Select value", 0, 100, 20)
 	window.title("So TIRED...")
 	frame.pack(expand=True, fill='x')
 	window.mainloop()
