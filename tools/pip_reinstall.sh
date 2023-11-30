6acc2364c3dafd9f74b40e1135446bf94487c42e
DmitryMurashov <dmtr.murashov@gmail.com>
2023-11-30 18:59:11 +0300

    [tools:pip_reinstall,pip_venv_fast_reinstall] Build | Reinstallation scripts

--- tools/pip_reinstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Just replace the package in venv
-rm -rf ./venv/lib/python3.9/site-packages/tired
-cp -r tired ./venv/lib/python3.9/site-packages/tired
--- tools/pip_venv_fast_reinstall.sh
+++ tools/pip_venv_fast_reinstall.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+# Just replace the package in venv
+rm -rf ./venv/lib/python3.9/site-packages/tired
+cp -r tired ./venv/lib/python3.9/site-packages/tired
