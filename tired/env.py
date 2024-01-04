ed91049b4032b1c3891415828e8623a29548755c
DmitryMurashov <dmtr.murashov@gmail.com>
2024-01-04 18:17:30 +0300

    [tired:env] Bug | Unscreened char

--- tired/env.py
+++ tired/env.py
@@ -5,7 +5,7 @@ import tired.fs
 import tired.logging
 
 
-def try_get_env(variable_name, accepted_values=None, panic_if_missing=False):
+def try_get_env(variable_name, accepted_values=None, panic_if_missing=False, type_=None):
     """
     - accepted_values: if iterable, the value will be checked against those in
       the list. If does not check, an exception will be raised
@@ -24,9 +24,17 @@ def try_get_env(variable_name, accepted_values=None, panic_if_missing=False):
 
         raise ValueError(message)
 
+    if type_ is not None:
+        try:
+            value = type_(value)
+        except ValueError as e:
+            tired.logging.error(f'Expected environment variable "{variable_name}" of type {type_}, failed to cast')
+
+            raise e
+
     if value is not None:
         tired.logging.info(f'Got environment variable {variable_name}="{value}"')
     else:
-        tired.logging.info(f'Didn't get environment variable "{variable_name}", continuing')
+        tired.logging.info(f'Didn\'t get environment variable "{variable_name}", continuing')
 
     return value
