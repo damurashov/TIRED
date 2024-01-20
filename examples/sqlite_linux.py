dd212dd9f7ed0217a8c815b17972091b51afd11b
DmitryMurashov <dmtr.murashov@gmail.com>
2024-01-20 20:04:31 +0300

    [examples:sqlite_linux] Impl |

---
+++ examples/sqlite_linux.py
@@ -0,0 +1,46 @@
+import tired.sqlite
+
+f1 = tired.sqlite.InfoField("quantity", int)
+f2 = tired.sqlite.InfoField("name", str)
+
+parent = tired.sqlite.Table("parent")
+child = tired.sqlite.Table("child")
+
+parent.add_field(f1)
+parent.add_field(f2)
+child_parent_field = tired.sqlite.ForeignIdField(parent)
+child.add_field(child_parent_field)
+child.add_field(f1)
+
+generate_db = tired.sqlite.GenerateDbScript()
+generate_db.add_table(parent)
+generate_db.add_table(child)
+print(generate_db.generate_sql_script())
+
+db = tired.sqlite.Db([parent, child])
+db.connect("/tmp/db.db")
+print(db.execute_script(generate_db))
+
+insert = tired.sqlite.InsertQuery(parent)
+insert.add_value(f1, "12")
+insert.add_value(f2, "some string")
+print(insert.generate_sql())
+print(db.execute(insert))
+
+insert = tired.sqlite.InsertQuery(child)
+insert.add_value(f1, "someone")
+insert.add_value(child_parent_field, 1)
+print(insert.generate_sql())
+print(db.execute(insert))
+
+query = tired.sqlite.InnerJoinSelectQuery(child)
+query.add_field(parent, f1)
+query.add_field(child, f1)
+print(query.generate_sql())
+print(db.execute(query))
+
+query = tired.sqlite.InnerJoinSelectQuery(parent)
+query.add_field(parent, f1)
+query.add_field(parent, f2)
+print(query.generate_sql())
+print(db.execute(query))
