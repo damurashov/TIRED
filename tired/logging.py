7c2765daf480510bdf1aad3d9718d190b9cf053b
DmitryMurashov <dmtr.murashov@gmail.com>
2024-03-03 14:52:49 +0300

    [tired] Ref | Refactored logging: split

--- tired/logging.py
+++ tired/logging.py
@@ -119,27 +119,9 @@ class _CountedQueue:
             yield from filter(lambda i: i.timestamp == timestamp, self._queue)
 
 
-class NetworkLoggingServer(socketserver.BaseRequestHandler):
-    instance = None
-    host = None
-    port = None
+class NetworkLoggingHandler(socketserver.BaseRequestHandler):
     _queue = _CountedQueue()
     _lock = threading.Lock()
-
-    @staticmethod
-    def send(data):
-        data = NetworkLoggingMessage.make_write_request_from_payload(data).as_str()
-
-        # Create a socket (SOCK_STREAM means a TCP socket)
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            try:
-                # Connect to server and send data
-                sock.connect((NetworkLoggingServer.host, NetworkLoggingServer.port))
-                sock.sendall(bytes(data + "\n", "utf-8"))
-            except ConnectionRefusedError as e:
-                pass
-            except BrokenPipeError as e:
-                pass
 
     def append_to_queue(self, item: str):
         self._queue.push(item)
@@ -189,21 +171,70 @@ class NetworkLoggingServer(socketserver.BaseRequestHandler):
 
             self._lock.release()
 
-    def run(self, host='localhost', port=8080):
-        # Create the server, binding to localhost on port 9999
-        with socketserver.TCPServer((NetworkLoggingServer.host, NetworkLoggingServer.port), NetworkLoggingServer) as server:
-            # Activate the server; this will keep running until you
-            # interrupt the program with Ctrl-C
-            server.serve_forever()
+    def send(data):
+        data = NetworkLoggingMessage.make_write_request_from_payload(data).as_str()
+
+        # Create a socket (SOCK_STREAM means a TCP socket)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            try:
+                # Connect to server and send data
+                sock.connect((self.host, self.port))
+                sock.sendall(bytes(data + "\n", "utf-8"))
+            except ConnectionRefusedError as e:
+                pass
+            except BrokenPipeError as e:
+                pass
+
+
+class NetworkLoggingServer:
+    instance = None
 
     @staticmethod
-    def send_message(level, context, *args):
+    def get_instance(host=None, port=None):
+        if NetworkLoggingServer.instance is None:
+            if host is None or port is None:
+                raise ValueError(f'Incorrect host/port pair ({host},{port})')
+
+            NetworkLoggingServer.instance = NetworkLoggingServer(host, port)
+
+        return NetworkLoggingServer.instance
+
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+
+    def get_host(self):
+        return self.host
+
+    def get_port(self):
+        return self.port
+
+    def send(self, data):
+        data = NetworkLoggingMessage.make_write_request_from_payload(data).as_str()
+
+        # Create a socket (SOCK_STREAM means a TCP socket)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            try:
+                # Connect to server and send data
+                sock.connect((self.host, self.port))
+                sock.sendall(bytes(data + "\n", "utf-8"))
+            except ConnectionRefusedError as e:
+                pass
+            except BrokenPipeError as e:
+                pass
+
+    def run(self, host='localhost', port=8080):
+        with socketserver.TCPServer((self.host, self.port), NetworkLoggingHandler) as server:
+            server.serve_forever()
+
+    def send_message(self, level, context, *args):
         global _LOG_SECTION_DELIMETER
         global LOG_LEVEL_TO_STRING_MAPPING
         message = ' '.join(args)
         output = ' '.join([LOG_LEVEL_TO_STRING_MAPPING[level], _LOG_SECTION_DELIMETER,
             f"{tired.datetime.get_today_time_milliseconds_string()}", f"[{context}]", _LOG_SECTION_DELIMETER, message])
-        NetworkLoggingServer.send(output)
+        self.send(output)
+
 
 
 def default_printer(level, context, *args):
@@ -216,28 +247,22 @@ def default_printer(level, context, *args):
 _PRINTER = default_printer
 
 
-def set_printer_network(host="localhost", port=8010):
-
-    if NetworkLoggingServer.instance is not None:
-        return
-
-    import threading
+def set_printer_remote_logging_server(host="localhost", port=8010):
+    instance = NetworkLoggingServer.get_instance(host=host, port=port)
     global _PRINTER
-    server = NetworkLoggingServer
-    NetworkLoggingServer.host = host
-    NetworkLoggingServer.port = port
-    thread = threading.Thread(target=server.run, args=(host,port))
-    NetworkLoggingServer.instance = thread
-    NetworkLoggingServer.instance.start()
-    _PRINTER = server.send_message
+    _PRINTER = instance.send_message
 
 
-def iter_connect_read_printer_network(counter, host="localhost", port=8080, read_counter=4096):
+def run_logging_server(host="localhost", port=8010):
+    """ Won't return """
+    NetworkLoggingServer.get_instance(host, port).run()
+
+
+def iter_connect_read_printer_network(counter, read_counter=4096):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         try:
             # Connect to server and send data
-            sock.connect((host, port))
-
+            sock.connect((NetworkLoggingServer.get_instance().get_host(), NetworkLoggingServer.get_instance().get_port()))
             request = NetworkLoggingMessage.make_read_request(timestamp=counter)
             sock.send(request.as_bytes())
 
