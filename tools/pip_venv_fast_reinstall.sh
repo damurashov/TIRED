2cb56d2c3ea719d570d335fae066bcaaea92c39e
DmitryMurashov <dmtr.murashov@gmail.com>
2024-06-08 15:04:59 +0300

    [tools] Impl | Fast reinstall

--- tools/pip_venv_fast_reinstall.sh
+++ tools/pip_venv_fast_reinstall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 # Just replace the package in venv
-rm -rf ./venv/lib/python3.9/site-packages/tired
-cp -r tired ./venv/lib/python3.9/site-packages/tired
+PROJECT=tired
+DIR=$(find venv/ -type d -name $PROJECT | tr -d '\n')
+rm -rf $DIR
+cp -r tired $DIR
